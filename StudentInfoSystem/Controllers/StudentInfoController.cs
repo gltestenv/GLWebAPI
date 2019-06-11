4b82960bee628f6e935b57a9c8f1235cc230014e
Ambrish Kumar Singh (GlobalLogic India Limited) <v-ambsi@microsoft.com>
2019-06-11 18:10:04 +0530

    Added StudentInfoController and GetStudentDetails

---
+++ StudentInfoSystem/Controllers/StudentInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentInfoSystem.Controllers
+{
+    public class StudentInfoController : ApiController
+    {
+		public IEnumerable<string> GetStudentDetails()
+		{
+			return new string[] { "Ambrish","Singh"};
+		}
+    }
+}
