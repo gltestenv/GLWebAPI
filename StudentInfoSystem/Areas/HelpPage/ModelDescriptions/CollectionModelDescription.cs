21860b558456667821d4ad786dc4bbc14a806e94
Ambrish Kumar Singh (GlobalLogic India Limited) <v-ambsi@microsoft.com>
2019-06-11 17:58:14 +0530

    Add project files.

---
+++ StudentInfoSystem/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace StudentInfoSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ StudentInfoSystem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StudentInfoSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
